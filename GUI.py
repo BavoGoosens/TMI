c263095a7d1b30930adc923b0601fd772ab9a435
Bavo Goosens <bavo.goosens@gmail.com>
2013-05-02 23:06:37 +0200

    partial working gui

--- GUI.py
+++ GUI.py
@@ -7,31 +7,48 @@ class GUI(object):
     """description of class"""
 
     def __init__(self,master):
+        self.cirkels = []
         frame = Frame(master,width=500, height=500)
         frame.pack()
-        self.entry = Entry(frame)
-        self.entry.bind("<return>", self.process())
-        self.entry.pack() 
+        self.master = master
+        self.entrytext = StringVar()
+        self.entry = Entry(master, textvariable=self.entrytext, width = 50).pack()
+        self.buttontext = StringVar()
+        self.buttontext.set("Start")
+        Button(master, textvariable=self.buttontext, command=self.clicked1).pack()
         self.canvas = Canvas(frame)
         self.canvas.pack()
+        self.labeltext = StringVar()
+        self.label = Label(frame, text = self.labeltext).pack()
+
+
         
     
 
-    def process(self):
-        path = self.entry.get()
+    def process(self,path):
         count = 0
-        cirkels = []
-        if len(path) > 4 :
-            for line in fileinput.input(files = (path)):
-                if count == 0:
-                     algo = line[0]
-                else:
-                    if len(line) > 2:
-                        cir = line.split( ' ' , 2)
-                        pos = Position( float(cir[0]), float(cir[1]))
-                        cir = Circle( pos , float(cir [2]))
-                        print(cir.to_string())
-                        cirkels.append(cir)
+        for line in fileinput.input(files = (path)):
+            if count == 0:
+                 algo = line[0]
+            else:
+                if len(line) > 2:
+                    cir = line.split( ' ' , 2)
+                    pos = Position( float(cir[0]), float(cir[1]))
+                    cir = Circle( pos , float(cir [2]))
+                    self.cirkels.append(cir)
             count = count + 1 
+
+    def clicked1(self):
+        input = self.entrytext.get()
+        self.process(input)
+        for circle in self.cirkels:
+            self.labeltext.set(self.labeltext.get() +"\n" + circle.to_string())
+
+        self.master.update_idletasks()
+
+        
+
+    def button_click(self, e):
+        pass
 
 

9a1979d23436823bc2b0e0e4ac78ff20f94b5dbb
Bavo Goosens <bavo.goosens@gmail.com>
2013-05-11 13:58:52 +0200

    coole kleurtjes

--- Circle.py
+++ Circle.py
@@ -2,12 +2,25 @@ import matplotlib.pyplot as plt
 
 class Circle(object):
     """A class used to represent circles"""
-    
+    color = 1 
+
+
     def __init__(self, center, radius):
         self.center = center
         self.radius = float(radius)
-        self.plot = plt.Circle((self.getCenter().getX(),self.getCenter().getY()),radius,color='b',fill=False)
- 
+        if (Circle.color % 4 == 0):
+            self.plot = plt.Circle((self.getCenter().getX(),self.getCenter().getY()),radius,color='b',fill=False) 
+            Circle.color = Circle.color + 1
+        elif (Circle.color % 4 == 1):
+            self.plot = plt.Circle((self.getCenter().getX(),self.getCenter().getY()),radius,color='g',fill=False)
+            Circle.color = Circle.color + 1
+        elif (Circle.color % 4 == 3):
+            self.plot = plt.Circle((self.getCenter().getX(),self.getCenter().getY()),radius,color='r',fill=False)
+            Circle.color = Circle.color + 1
+        else:
+            self.plot = plt.Circle((self.getCenter().getX(),self.getCenter().getY()),radius,color='black',fill=False)
+            Circle.color = Circle.color + 1
+
     def display(self):
         print ("Center: ", self.center)
         print ("Radius: ", self.radius)

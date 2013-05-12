1986086d6cc3cebffbdac259f52a856e72d99416
Bavo Goosens <bavo.goosens@gmail.com>
2013-05-12 21:58:42 +0200

    algo 1 af moet nog getest worden en de intersect method moet nog geschreven worden

--- Solver.py
+++ Solver.py
@@ -18,19 +18,23 @@ class Solver(object):
 
     def algo1(self):
         #do shizlle in O(N^2)
+        intersections = []
         for circle in self.cirkels:
             hulp = list(self.cirkels)
             hulp.remove(circle)
             for otherCircle in hulp:
                 dis = circle.center_distance(otherCircle)
-                if not ( dis > (circle.getRadius() + otherCircle.getRadius()) or d < abs(cirle.getRadius() - otherCircle.getRadius()) or (dis == 0 and circle.getRadius() == otherCircle.getRadius()):
-
-
+                if not ( dis > (circle.getRadius() + otherCircle.getRadius())) and not (d < abs(cirle.getRadius() - otherCircle.getRadius())) and not (dis == 0 and circle.getRadius() == otherCircle.getRadius()):
+                        intersect = circle.intersect(otherCircle)
+                        if not (intersect in intersections):
+                            intersections.append(intersect)
                 else:
                     # geen oplossingen cirkels liggen te ver van elkaar
                     # geen oplossingen cirkels liggen omvat in elkaar 
                     # oneindig veel oplossingen samenvallende cirkels
-                    if (
+                    if (dis == 0 and circle.getRadius() == otherCircle.getRadius()):
+                        return 'infinity'
+        return intersections
 
     def algo2(self):
         #do shizzle in O(N^2) maar verhoog de effici?ntie met doorlooplijn

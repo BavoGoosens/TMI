0b6cc0815e1577c98aa50927e452eb9b89082613
Bavo Goosens <bavo.goosens@gmail.com>
2013-05-14 16:00:43 +0200

    shizzles

--- GUI.py
+++ GUI.py
@@ -15,33 +15,50 @@ class GUI(object):
 
         # de parent van deze GUI = root
         self.master = master
-
-        # bottom en top frames om zo de layout wat te verbeteren
-        self.top = Frame(master)
-        self.bottom = Frame(master)
-        self.top.pack(side=TOP)
-        self.bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
         
+        # bottom en top frames om zo de layout wat te verbeteren
+        self.top = Frame(master, bd=1,relief=SUNKEN,padx=1,pady=1)
+        self.bottom = Frame(master, bd=1,relief=SUNKEN,padx=1,pady=1)
+        self.top.pack(side=TOP,fill=BOTH, expand=True)
+        self.bottom.pack(side=BOTTOM, fill=BOTH, expand=True)
+
+        # top in nog meer frames opdelen links en rechts
+        self.leftTOP = Frame(self.top, bd=1,relief=SUNKEN,padx=1,pady=1)
+        self.rightTOP = Frame(self.top, bd=1,relief=SUNKEN,padx=1,pady=1)
+        self.leftTOP.pack(side=LEFT,fill=BOTH, expand=True)
+        self.rightTOP.pack(side=RIGHT,fill=BOTH, expand=True)
+
+        # uitleg
+        w = Label(self.rightTOP, text="Enter the path to to input file in the first field"+"\n" +"Enter the path to the output file in the second field" + "\n" + "push the START button to start")
+        w.pack()
+
         # buttons 
         self.buttontext = StringVar()
-        self.buttontext.set("Start")
-        self.startbutton = Button(self.master, textvariable=self.buttontext, command=self.clicked1)
+        self.buttontext.set("START")
+        self.startbutton = Button(master, textvariable=self.buttontext, command=self.clicked1,height = 2)
+        
         self.buttontext1 = StringVar()
         self.buttontext1.set("Browse")
-        self.browsebutton = Button(self.master, textvariable=self.buttontext1)
-        self.startbutton.pack(in_= self.top, side = LEFT)
-        self.browsebutton.pack(in_= self.top, side = LEFT)
-
-        self.inputframe = Frame(master,width=300, height=500)
-        self.inputframe.pack(in_= self.bottom, side = LEFT)
-        self.outputframe = Frame(master,width=300, height=500)
-        self.outputframe.pack(in_= self.bottom, side = LEFT)
-        self.entrytext = StringVar()
-        self.entry = Entry(self.top, textvariable=self.entrytext, width = 50).pack()
-
-
+        self.browsebutton = Button(master, textvariable=self.buttontext1, height = 1)
         
-    
+        self.startbutton.pack(in_= self.rightTOP)
+        self.browsebutton.pack(in_= self.leftTOP, side = RIGHT)
+
+        self.inputframe = Frame(master,width=500, height=500, bd=1,relief=SUNKEN,padx=1,pady=1,bg="blue")
+        self.inputframe.pack(in_= self.bottom, side = LEFT)
+        
+        self.outputframe = Frame(master,width=500, height=500, bd=1,relief=SUNKEN,padx=1,pady=1,bg="blue")
+        self.outputframe.pack(in_= self.bottom, side = LEFT)
+
+        #entry fields 
+        self.entrytext = StringVar()
+        self.inputEntry = Entry(master, textvariable=self.entrytext, width = 50).pack(in_= self.leftTOP)
+
+        self.entrytext1 = StringVar()
+        self.outputEntry = Entry(master, textvariable=self.entrytext1, width = 50).pack(in_= self.leftTOP)
+
+    def output(self, path):
+         print (path)
 
     def process(self,path):
         count = 0
@@ -61,8 +78,9 @@ class GUI(object):
         self.intersections = self.solver.find_intersect()
 
     def clicked1(self):
-        input = self.entrytext.get()
-        self.process(input)
+
+        self.process(self.entrytext.get())
+        self.output(self.entrytext1.get())
         
         var = StringVar()
         var.set('Uw cirkels:')
@@ -80,7 +98,7 @@ class GUI(object):
         var1.set('Snijpunten:')
 
         interlabel = Label(self.outputframe, textvariable = var1)
-        cirkelabel.pack()
+        interlabel.pack()
 
         for inter in self.intersections[0]:
             var1.set(var1.get() +"\n" + inter.to_string())
--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from tkinter import *
 
 def main():
     root = Tk()
-    w = Label(root, text="Enter the path to to input file and push the start button")
-    w.pack()
     gui = GUI(root)
     root.title("Bavo and Sander: Circle Processor")
     root.mainloop()
